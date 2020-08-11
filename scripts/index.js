31ae0525de745c8b1aa8435973c5ca9c622a938b
Namullan <68558793+Namullan@users.noreply.github.com>
2020-08-10 23:11:20 -0300

    fundo escurecido nos cards - added

--- scripts/index.js
+++ scripts/index.js
@@ -22,44 +22,57 @@ $(".erros-popup-card").fadeOut().delay(10).queue(function() {
     $(".erros-popup-card").dequeue();
 });
 
+$(".escurecer-fundo").fadeOut().delay(10).queue(function() {
+    $(".escurecer-fundo").removeClass("invisible");
+    $(".escurecer-fundo").dequeue();
+});
+
 // Dúvidas
 $(".duvidas-icon").click(function() {
     $(".erros-popup-card").fadeOut();
+    $(".escurecer-fundo").fadeIn();
     $(".duvidas-card").fadeIn();
 });
 
 $(".duvidas-btn").click(function() {
     $(".duvidas-card").fadeOut();
+    $(".escurecer-fundo").fadeOut();
 });
 
 // Desorganização
 $(".desorganizacao-icon").click(function() {
     $(".erros-popup-card").fadeOut();
+    $(".escurecer-fundo").fadeIn();
     $(".desorganizacao-card").fadeIn();
 });
 
 $(".desorganizacao-btn").click(function() {
     $(".desorganizacao-card").fadeOut();
+    $(".escurecer-fundo").fadeOut();
 });
 
 // Desinteresse
 $(".desinteresse-icon").click(function() {
     $(".erros-popup-card").fadeOut();
+    $(".escurecer-fundo").fadeIn();
     $(".desinteresse-card").fadeIn();
 });
 
 $(".desinteresse-btn").click(function() {
     $(".desinteresse-card").fadeOut();
+    $(".escurecer-fundo").fadeOut();
 });
 
 // Desmotivação
 $(".desmotivacao-icon").click(function() {
     $(".erros-popup-card").fadeOut();
+    $(".escurecer-fundo").fadeIn();
     $(".desmotivacao-card").fadeIn();
 });
 
 $(".desmotivacao-btn").click(function() {
     $(".desmotivacao-card").fadeOut();
+    $(".escurecer-fundo").fadeOut();
 });
 
 // ------------------ Back to top button -----------------
